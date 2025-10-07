8971b684aa1395b210f70171776c50efe8c5adf1
Ala Williams <alicja.m.williams@gmail.com>
2025-10-07 13:40:14 +0300

    GET all public sessions and POST session  method

--- backend/data/db.js
+++ backend/data/db.js
@@ -12,15 +12,18 @@ db.exec(`
     time TEXT NOT NULL,
     maxParticipants INTEGER NOT NULL,
     isPublic BOOLEAN NOT NULL DEFAULT 0,
-    managementCode TEXT NOT NULL
+    managementCode TEXT NOT NULL,
+    privateCode TEXT UNIQUE
   );
 
-  CREATE TABLE IF NOT EXISTS participants(
+  CREATE TABLE IF NOT EXISTS participants (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     sessionId INTEGER NOT NULL,
     name TEXT NOT NULL,
     isEditor BOOLEAN NOT NULL DEFAULT 0,
     attendanceCode TEXT NOT NULL,
-    FOREIGN KEY(sessionId) REFERENCES sessions(id),
-);
-`)
+    FOREIGN KEY(sessionId) REFERENCES sessions(id)
+  );
+`);
+
+module.exports = db;
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,78 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
-const port = 3000
+const port = 3000;
+const db = require('./data/db');
+
 app.use(cors())
+app.use(express.json())
+
 
 app.listen(port, () => {
   console.log(`Server is running at port ${port}`)
 })
+
+// GET /sessions → list all public sessions
+app.get('/sessions', (req, res) => {
+  const sessions = db.prepare('SELECT * FROM sessions WHERE isPublic = 1').all()
+  res.json(sessions)
+})
+
+// GET /sessions/:id → get details of one session
+app.get('/sessions/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const session = db.prepare('SELECT * FROM sessions WHERE id = ?').get(id)
+  
+  if (session) {
+    res.json(session)
+  } else {
+    res.status(404).json({
+      error: 'Session not found'
+    })
+  }
+})
+
+
+// POST /sessions → create a new session (and generate a management code)
+app.post('/sessions', (req, res) => {
+  const { title, description, category, date, time, maxParticipants, isPublic } = req.body
+
+  const managementCode = Math.random().toString(36).substring(2,8)
+  const privateCode = isPublic ? null : Math.random().toString(36).substring(2,8);
+
+  
+
+  const insert = db.prepare(`
+    INSERT INTO sessions (title, description, category, date, time, maxParticipants, isPublic, managementCode, privateCode)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+  `)
+
+  const result = insert.run (
+    title,
+    description,
+    category,
+    date,
+    time,
+    maxParticipants,
+    isPublic,
+    managementCode, 
+    privateCode
+  )
+  res.status(201).json({
+    success: true,
+    message: 'Session created successfully',
+    sessionId: result.lastInsertRowid,
+    managementCode,
+    privateCode,
+  }
+)})
+
+
+// PUT /sessions/:id → edit a session (requires management code)
+
+// DELETE /sessions/:id → delete a session (requires management code)
+
+// Participants
+// 6. POST /sessions/:id/join → join a session (generates attendance code)
+// 7. DELETE /sessions/:id/leave → leave a session using attendance code
+// 8. DELETE /sessions/:id/remove → remove a participant (requires management code)
